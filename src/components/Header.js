3cf296f54286b8f483098db00748556cf606002b
Rohit Kumar <07nith.me@gmail.com>
2024-03-02 01:13:45 +0530

    integrated tailwind and added header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,17 @@
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className="border-b-2 p-4 flex justify-between">
+            <div className="text-4xl font-bold">
+                <span className="text-yellow-400">blink</span>
+                <span className="text-green-400">it</span>
+            </div>
+            <div className="">
+                <input className="rounded-lg p-4 border-gray-500" type="text" placeholder="Search for products"/>
+            </div>
+            <div className="flex justify-between">
+                <button className="mx-4">Login</button>
+                <button className="mx-4">Cart</button>
+            </div>
         </div>
     )
 }

4be84d309029d3344b81ab7aa031024ecc7c01fe
Rohit Kumar <07nith.me@gmail.com>
2024-03-02 01:21:10 +0530

    refactored header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 const Header = () => {
     return (
         <div className="border-b-2 p-4 flex justify-between">
-            <div className="text-4xl font-bold">
+            <div className="text-4xl font-extrabold">
                 <span className="text-yellow-400">blink</span>
                 <span className="text-green-400">it</span>
             </div>
             <div className="">
-                <input className="rounded-lg p-4 border-gray-500" type="text" placeholder="Search for products"/>
+                <input className="rounded-lg p-4 border-gray-500" 
+                    type="text" 
+                    placeholder="Search for products"/>
             </div>
             <div className="flex justify-between">
                 <button className="mx-4">Login</button>

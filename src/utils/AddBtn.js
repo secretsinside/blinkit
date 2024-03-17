db6b7720bca0c795c11df076d8f5f00f59a31724
Rohit Kumar <07nith.me@gmail.com>
2024-03-18 05:03:41 +0530

    added addBtn utility component

---
+++ src/utils/AddBtn.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+const AddBtn = () => {
+    const [item, setItem] = useState(0);
+
+    function handleAdd() {
+        setItem(item+1);
+    }
+
+    function handleRemove() {
+        setItem(item-1);
+    }
+
+    return (
+        <>
+            {
+                item == 0 ?
+                    <button className="text-xs border-x border-y border-green-600 rounded-md p-1 text-green-600 font-bold w-14"
+                        onClick={handleAdd}>
+                        ADD
+                    </button>
+                    :
+                    <div className="text-xs border-x border-y bg-green-600 rounded-md p-1 text-white font-bold w-14 flex justify-between">
+                        <button className="px-1"
+                            onClick={handleRemove}>-</button>
+                        <span>{item}</span>
+                        <button className="px-1"
+                            onClick={handleAdd}>+</button>
+                    </div>
+            }
+            
+        </>
+    )
+}
+
+export default AddBtn;

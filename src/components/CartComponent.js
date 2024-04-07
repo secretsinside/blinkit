093f71f495687cde38dcdf280074041b4d38ee8a
Rohit Kumar <07nith.me@gmail.com>
2024-04-07 16:09:07 +0530

    added view saved cart component for viewing saved carts

--- src/components/CartComponent.js
+++ src/components/CartComponent.js
@@ -1,14 +1,16 @@
-import { FaCircleXmark, FaFile, FaBicycle, FaCaretRight } from "react-icons/fa6";
+import { FaCircleXmark, FaArrowLeft, FaFile, FaBicycle, FaCaretRight } from "react-icons/fa6";
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import ItemCard from "./ItemCard";
+import SaveCartComponent from "./SaveCartComponent";
+import ViewSavedCart from "./ViewSavedCart";
 
 const CartComponent = ({closeCartModal}) => {
 
     const {items, selectedItemId, totalMrp, totalDiscountedPrice } = useSelector((store) => store.cart);
     const [selectedItems, setSelectedItems] = useState(null);
-    const [saveCart, setSaveCart] = useState(false);
     const deliveryCharge = useState(0);
+    const [showSavedCartComponent, setShowSavedCartComponent] = useState(false);
     const [grandTotal, setGrandTotal] = useState(0);
 
 
@@ -20,9 +22,12 @@ const CartComponent = ({closeCartModal}) => {
         setGrandTotal(parseInt(deliveryCharge)+parseInt(totalDiscountedPrice));
     }, [totalMrp]);
 
-    function toggleSaveCart(e) {
-        e.stopPropagation();
-        setSaveCart(!saveCart);
+    function showSavedCart() {
+        setShowSavedCartComponent(true);
+    }
+
+    function hideSavedCart() {
+        setShowSavedCartComponent(false);
     }
 
     return (
@@ -37,7 +42,27 @@ const CartComponent = ({closeCartModal}) => {
                         onClick={closeCartModal}><FaCircleXmark className="text-lg"/></button>
                 </div>
                 <div className="p-4">
+                    <div className="bg-white rounded-md px-2 text-center">
+                        {
+                            showSavedCartComponent ?
+                            <div className="text-left flex items-center p-2">
+                                <button 
+                                    className="py-2"
+                                    onClick={hideSavedCart}>
+                                    <FaArrowLeft className="text-green-700 text-lg"/>
+                                </button>
+                                <span className="mx-4 font-bold text-green-700">Showing saved carts</span>
+                            </div>
+                            :
+                            <button className="my-4 bg-green-700 text-white text-sm font-medium p-2 rounded-md w-full"
+                            onClick={showSavedCart}>
+                                Saved cart
+                            </button>
+                        }
+
+                    </div>
                     {
+                        showSavedCartComponent ? <ViewSavedCart hideSaveCartView={hideSavedCart}/> :
                         selectedItems?.length ?
                         <>
                             <div className="w-full my-5 rounded-md bg-white py-2">
@@ -66,24 +91,7 @@ const CartComponent = ({closeCartModal}) => {
                                 </p>
                             </div>
 
-                            <div className="w-full my-5 rounded-md bg-white p-2">
-                                <div className="hover:cursor-pointer" onClick={toggleSaveCart}>
-                                    <input className="accent-green-700" type="checkbox" checked={saveCart}/>
-                                    <span className="px-2">Save this cart</span>
-                                </div>
-                                {
-                                    saveCart && (
-                                        <div className="my-2 border-x border-y rounded-lg p-2 flex-column">
-                                            <label className="text-xs" for="save-cart">Name your cart</label>
-                                            <div className="my-2 flex justify-between">
-                                                <input className="p-2 mr-2 focus:outline-green-700  w-full" id="save-cart" type="text" placeholder="Eg. Breakfast"/>
-                                                <button className="p-2 px-4 bg-green-700 text-white rounded-md text-sm">Save</button>
-                                            </div>
-                                        </div>
-                                    )
-                                }
-                                
-                            </div>
+                            <SaveCartComponent/>
 
                             <div className="w-full my-5 rounded-md bg-white p-2">
                                 <button className="w-full my-4 bg-green-700 text-white text-sm py-4 px-2 rounded-md flex items-center font-bold">
---
+++ src/components/ViewSavedCart.js
@@ -0,0 +1,10 @@
+const ViewSavedCart = ({hideSaveCartView}) => {
+
+    return (
+        <div>
+            Saved cart detail goes here!
+        </div>
+    )
+}
+
+export default ViewSavedCart;
